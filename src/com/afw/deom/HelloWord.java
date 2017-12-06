ac90a0166efeca8fd4c67c6d13b2349084cd7a17
wen157604891 <157604891@qq.com>
2017-12-06 15:33:07 +0800

    mater提交

--- src/com/afw/deom/HelloWord.java
+++ src/com/afw/deom/HelloWord.java
@@ -3,7 +3,7 @@ package com.afw.deom;
 public class HelloWord {
 
 	public static void main(String[] args) {
-		System.out.println("main方法打印Master提交");
+		System.out.println("main方法打印Mas提交");
 		System.out.println("我是test提交");
 	}
 }
